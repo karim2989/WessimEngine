6f3e8639c367e96d052b99297a2753aaf082ff1a
karim2989 <karim2989@gmail.com>
2020-10-05 17:44:10 +0100

    5 oct 20

---
+++ FlirtGame/Editor/EditorScreen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine;
+using Engine.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace FlirtGame
+{
+    class EditorScreen : Engine.Screen
+    {
+        public Canvas Canvas;
+
+        public EditorScreen(Game g,GraphicsDeviceManager graphics) : base(g)
+        {
+            Canvas = new Canvas(new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight));
+        }
+
+        public override void Start()
+        {
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, GraphicsDevice graphicsDevice, SpriteBatch sb)
+        {
+            sb.Begin();
+
+            Canvas.Draw(sb);
+
+            sb.End();
+        }
+    }
+}
---
+++ FlirtGame/Editor/ScenarioWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Engine;
+using Engine.UI;
+using FlirtGame;
+using UI = Engine.UI;
+
+namespace FlirtGame.Editor
+{
+    public class ScenarioWriter
+    {
+
+    }
+    public class ScreenWriter
+    {
+        public List<ScreenElement> Elements;
+        public Container Container { get; set; }
+
+        public ScreenWriter()
+        {
+
+        }
+    }
+    public abstract class ScreenElement
+    {
+        public ScreenWriter Parent { get; set; }
+        public int PositionInParent { get; set; }
+        public abstract UI.Container Draw();
+        public abstract string Serialize();
+    }
+}
